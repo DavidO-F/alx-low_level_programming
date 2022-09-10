66532a10298822248e07099356c3ebff16386c05
Dave <daveteletech.webdev2021@gmail.com>
2022-09-09 17:20:46 -0700

    C program that prints some texts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The main is the entering point of the progarm
+ * Return: zero which is the exit point in the function main
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
