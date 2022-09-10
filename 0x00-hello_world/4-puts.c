e3e4f58dd7f37737c4beb0d38bf04247bde01dc5
Dave <daveteletech.webdev2021@gmail.com>
2022-09-09 17:10:04 -0700

    C program that print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the main block that begins the program entering point
+ * Return: an integer value is the return value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
