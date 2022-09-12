fdfe1f1fc3b3da3394ede4fd105c179b26a3a3ae
Dave <daveteletech.webdev2021@gmail.com>
2022-09-12 11:23:51 -0700

    3C- for 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,33 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - entering point of the program
- * Description: print some text
- * Return: return one
+ *
+ *  * main - Prints output
+ *
+ *   *
+ *
+ *    * Return: Always 0
+ *
  */
+
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	long l = 59;
-
-	long fd = 1;
-	
-	long syscall = 1;
-	
-	long ret = 0;
-
-	__asm__ ("syscall"
-
-			: "=a" (ret)
-
-			: "a" (syscall),
-
-			"D" (fd),
-			
-			"S" (s),
-
-			"d" (1));
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
