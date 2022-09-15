a27d5755e55444fc4d632dc3498f9ef7e4dd7af8
Dave <daveteletech.webdev2021@gmail.com>
2022-09-15 15:02:52 -0700

    Print number from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * print_number - a function that prints integer from 0 to 9
+ */
+
+void print_number(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+		i++;
+	}
+	_putchar('\n');
+}
